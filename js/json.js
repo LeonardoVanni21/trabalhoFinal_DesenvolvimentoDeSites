9e2fe0bc885f8e049dcfc1d403a498a77deccebd
Leonardo Vanni Bonavigo <leonardovanni21@gmail.com>
2023-06-17 15:06:37 -0300

    Commit Final

--- js/json.js
+++ js/json.js
@@ -11,3 +11,26 @@ const maskPhoneNumber = (value) => {
 const applyPhoneNumberMask = (input) => {
     input.value = maskPhoneNumber(input.value)
 }
+
+function makeJson() {
+    let nome = $("#nome").val();
+    let email = $("#email").val();
+    let telefone = $("#telefone").val();
+    let senha = $("#senha").val();
+    let json = {
+        "nome": nome,
+        "email": email,
+        "telefone": telefone,
+        "senha": senha
+    }
+    console.log(json);
+    $("#result").val(JSON.stringify(json));
+
+    $("#result").each(function () {
+        let json = $(this).val();
+        if (json != "") {
+            json = JSON.stringify(JSON.parse(json), undefined, 2);
+            $(this).val(json);
+        }
+    });
+}
