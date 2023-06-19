77411eabb2124fd26f3896042bd61faa7cf8fd2b
Leonardo Vanni Bonavigo <71037268+LeonardoVanni21@users.noreply.github.com>
2023-06-19 19:12:34 -0300

    Update json.js

--- js/json.js
+++ js/json.js
@@ -4,7 +4,7 @@ const maskPhoneNumber = (value) => {
     return value
         .replace(/[\D]/g, '')
         .replace(/(\d{2})(\d)/, '($1) $2')
-        .replace(value[5] != 9 ? /(\d{5})(\d)/ : /(\d{5})(\d)/, '$1-$2')
+        .replace(/(\d{5})(\d)/, '$1-$2')
         .replace(/(-\d{4})(\d+?)/, '$1')
 }
 
