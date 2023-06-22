5870c5946026cbca2981f9c232b3657fec06a7a7
Leonardo Vanni Bonavigo <71037268+LeonardoVanni21@users.noreply.github.com>
2023-06-22 20:35:32 -0300

    Ajuste Navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+window.onload = function () {
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf("/") + 1);
+  var menu = document.getElementById("menu");
+
+  if (filename == "imc.html") {
+    menu.children[1].classList.add("active");
+  } else if (filename == "calculadora.html") {
+    menu.children[0].classList.add("active");
+  } else if (filename == "json.html") {
+    menu.children[2].classList.add("active");
+  }
+}
+
 function redirectImc() {
   window.location.href = "imc.html";
 }
@@ -9,3 +23,4 @@ function redirectCalculadora() {
 function redirectJson() {
   window.location.href = "json.html";
 }
+
